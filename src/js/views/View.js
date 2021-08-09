7bf7a6e1067838fdf1d7026ceab47150bf75fca2
parvathypanicker <parvathypanicker@gmail.com>
2021-08-09 14:42:45 +1000

    Corrected code in View.js

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -70,14 +70,16 @@ export default class View {
   }
 
   renderMessage(message = this._message) {
-    const markup = `<div class="message">
-                <div>
-                  <svg>
-                    <use href="${icons}#icon-smile"></use>
-                  </svg>
-                </div>
-                <p>${message}</p>`;
-
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
